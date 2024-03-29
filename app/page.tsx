0db63018241855e792097a57e184c395e991f8b5
Gabriel Kieruzel <gabriel.kieruzel@protonmail.com>
2024-03-29 14:24:45 +0100

    another commit

--- app/page.tsx
+++ app/page.tsx
@@ -8,18 +8,18 @@ import Image from 'next/image';
 export default function Page() {
   return (
     <main className="flex min-h-screen flex-col p-6">
-      <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
+      <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-600 p-4 md:h-52">
         <AcmeLogo />
       </div>
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
 
           <p className={`text-xl text-gray-800 md:text-3xl md:leading-normal`}>
-            <strong>Welcome to Acme.</strong> This is the example for the{' '}
+            <strong>Witaj w Next js</strong> To przykład z oficjalnego samouczka (w wersji angielskiej){' '}
             <a href="https://nextjs.org/learn/" className="text-blue-500">
               Next.js Learn Course
             </a>
-            , brought to you by Vercel.
+            , przygotowanego przez zespół Vercel-a.
           </p>
           <Link
             href="/login"
@@ -29,20 +29,19 @@ export default function Page() {
           </Link>
         </div>
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
-          {/* Add Hero Images Here */}
           <Image
-          src="/hero-desktop.png" 
+          src="/Katowice-ACME.png" 
           width={1000}
           height={760}
-          className='hidden md:block'
-          alt='Screenshopts of the dashboard project showing desktop version'
+          className='rounded-xl shadow-lg hidden md:block'
+          alt='Artificial picture of Katowice showing desktop version'
           />
           <Image
-          src="/hero-mobile.png" 
+          src="/Katowice-ACME-mobile.png" 
           width={560}
           height={620}
-          className='block md:hidden'
-          alt='Screenshopts of the dashboard project showing mobile version'
+          className='rounded-xl shadow-lg block md:hidden'
+          alt='Artificial picture of Katowice showing mobile version'
           />
         </div>
       </div>
